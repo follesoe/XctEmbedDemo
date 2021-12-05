daecc90764ed4303d6566145d3f6a0c9a5825008
Jonas Follesø <jonas@follesoe.no>
2021-12-05 21:38:05 +0100

    Initial commit

---
+++ XctEmbedDemo/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XctEmbedDemo;
+
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow Window
+    {
+        get;
+        set;
+    }
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        Forms.Init();
+
+        // Create app-level resource dictionary.
+        Xamarin.Forms.Application.Current = new Xamarin.Forms.Application
+        {
+            MainPage = new Page()
+        };
+        Xamarin.Forms.Application.Current.Resources = new ResourceDictionary();
+
+        // create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        var mainViewController = new MainViewController();
+        var navController = new UINavigationController(mainViewController);
+        Window.RootViewController = navController;
+
+        // make the window visible
+        Window.MakeKeyAndVisible();
+
+        return true;
+    }
+}
---
+++ XctEmbedDemo/MainViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.CommunityToolkit.Extensions;
+
+namespace XctEmbedDemo
+{
+    public class MainViewController : UIViewController
+    {
+        private Popup popup;
+
+        public MainViewController()
+        {
+            popup = new Popup
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Hello Xct World!"
+                        }
+                    }
+                }
+            };
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+            Title = "Popup Demo";
+
+            var button = UIButton.FromType(UIButtonType.System);
+            button.Frame = new CGRect(20, 200, 280, 44);
+            button.SetTitle("Show Xct Popup", UIControlState.Normal);
+            button.TouchUpInside += OnTouchUpInside;
+
+            View.AddSubview(button);
+        }
+
+        private void OnTouchUpInside(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("Tapped button!");
+                Xamarin.Forms.Application.Current.MainPage.Navigation.ShowPopup(popup);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
